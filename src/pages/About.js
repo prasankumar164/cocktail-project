3b718d2711c18fe2faba30bafc734a4c5f0e88b2
unknown <viratprasan14@gmail.com>
2020-12-23 08:50:55 +0530

    cocktail project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptas suscipit odio quidem, dolor maiores praesentium autem similique voluptatibus, rerum temporibus doloribus, sequi sint eos est quod? Laudantium ipsa alias iure suscipit doloribus necessitatibus nemo laboriosam eligendi facere tenetur, excepturi magni, eos accusamus amet eius quia fuga repellat! Sunt, itaque!</p>
+    </section>
+  )
+}
+
+export default About
